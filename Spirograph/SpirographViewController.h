29e714e05b9aeeb1243cacd081c461dd6fc90a54
Michael Toth <michael@virtualpianist.com>
2014-02-20 19:32:02 -0500

    Initial Commit

---
+++ Spirograph/SpirographViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpirographViewController.h
+//  Spirograph
+//
+//  Created by Michael Toth on 2/20/14.
+//  Copyright (c) 2014 Michael Toth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpirographViewController : UIViewController
+
+@end
---
+++ Spirograph/SpirographViewController.m
@@ -0,0 +1,29 @@
+//
+//  SpirographViewController.m
+//  Spirograph
+//
+//  Created by Michael Toth on 2/20/14.
+//  Copyright (c) 2014 Michael Toth. All rights reserved.
+//
+
+#import "SpirographViewController.h"
+
+@interface SpirographViewController ()
+
+@end
+
+@implementation SpirographViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
